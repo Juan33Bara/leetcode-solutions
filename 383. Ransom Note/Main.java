dc2c327cee3574b227e2fa5243a5ba2827d7947c
Juan Cruz <4000109@sistemas.frc.utn.edu.ar>
2026-01-09 20:08:24 -0300

    firs commit

---
+++ 383. Ransom Note/Main.java
@@ -0,0 +1,13 @@
+
+class Main {
+
+    public static void main(String[] args) {
+        //System.out.println("Hello, World!");
+        Solution solu = new Solution();
+        String ransomNote = "aa";
+        String magazine = "aba";
+        boolean result = solu.canConstruct(ransomNote, magazine);
+        System.out.println(result);
+    }
+
+}
