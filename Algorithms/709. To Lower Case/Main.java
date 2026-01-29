d9b417b1fb5de202fbca0eb72f70d69bba4f8923
Juan Cruz <juanbaravalle33@gmail.com>
2026-01-29 10:37:18 -0300

    Solve 709.To Lower Case exercice

---
+++ Algorithms/709. To Lower Case/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        String result = sol.toLowerCase("HOLA");
+        System.out.println(result);
+        
+    }
+}
